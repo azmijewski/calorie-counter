9d32458422b585a0d929adb6c08e6df4244ec5cc
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-09-04 08:50:10 +0200

    Article page

--- src/app/articles-page/articles-page.component.ts
+++ src/app/articles-page/articles-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ArticleWebService} from '../services/web/article-web.service';
+import {ActivatedRoute} from '@angular/router';
+import {Article} from '../model/article';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-articles-page',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesPageComponent implements OnInit {
 
-  constructor() { }
+  article: Article;
+  isDataLoaded = false;
+  appUrl = environment.appUrl + 'photos/';
+
+  constructor(private articleService: ArticleWebService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.isDataLoaded = false;
+    this.route.params.subscribe(param => {
+      this.articleService.loadArticleById(param.id).subscribe(data => {
+        this.article = data;
+      }, error => {}, () => this.isDataLoaded = true);
+    });
   }
+
 
 }
